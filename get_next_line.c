b839478a9655da7a90a08a5becb991850426bcc7
warren fung <wfung@e1z2r6p1.42.us.org>
2017-04-02 20:58:12 -0700

    progress

--- get_next_line.c
+++ get_next_line.c
@@ -6,29 +6,30 @@ int		get_next_line(const int fd, char **line)
 	int				count;
 	char			*tmp;
 	static char		*buff;
-	int				what;
+	int				read_attempt;	//for testing purposes
 
 	buff = NULL;
-	if (fd == -1 || !(tmp = ft_memalloc((size_t)BUFF_SIZE)))
+	if (fd == -1 || !(tmp = ft_memalloc((size_t)BUFF_SIZE + 1)))
 		return (-1);
-	what = 1;
+	read_attempt = 1;
 	printf("inside GNL; about to start\n");
 	while ((count = (read(fd, tmp, BUFF_SIZE) > 0)))	//count bytes read
 	{												//might be diff vs BUFF_SIZE
 		tmp[count] = '\0';
-		printf("what = [%i] count = [%i]\n", what, count);
+		printf("read_attempt = [%i] count = [%i]\ntmp = %s\n", read_attempt, count, tmp);
 		buff == NULL ? buff = ft_strdup(tmp) : (buff = ft_strjoin(buff, tmp));
 		if (!buff)
 			return (-1);
+		printf("buff str = [%s]\n", buff);
 		if (ft_strchr(buff, 10) != NULL)
 			break;
 		ft_memalloc((size_t)tmp);		//clear tmp
-		what++;
+		read_attempt++;
 	}
 	printf("finished reading inside GNL\n");
 	free(tmp);
 	ft_memccpy(&line, buff, 10, ft_strlen(buff));
-	tmp = ft_memalloc((size_t)line);
+	tmp = ft_memalloc((size_t));
 	tmp = ft_strsub(buff, ft_strcmp(buff, *line), ft_strlen(buff) - ft_strcmp(buff, *line));
 	if (!tmp)
 		return (-1);
@@ -36,5 +37,7 @@ int		get_next_line(const int fd, char **line)
 		return (-1);
 	free(tmp);
 	printf("line str = [%s]\n", *line);
+	if (count == 0)
+		return (0);
 	return (1);
 }
--- main.c
+++ main.c
@@ -8,28 +8,28 @@ int		main(int ac, char **av)
 	int		fd_test;
 	int		i;
 	char	*store;
-	int		wee;
+	int		gnl_call;
 
 	i = 0;
-	wee = 1;
+	gnl_call = 1;
 	if (ac == 2)		//handling only 1 fd
 	{
 		fd_test = open(av[1], O_RDONLY);
 		while (get_next_line(fd_test, &store) == 1)
 		{
-			printf("nextline # = [%i]\n", wee);
-			wee++;
+			printf("nextline # = [%i]\n", gnl_call);
+			gnl_call++;
 		}	
 		close(fd_test);
 		i = 0;		//reset i
 		while (store != 0)	//print store
 		{
-			printf("%s\n", store);
+			printf("testing line str = %s\n", store);
 			i++;
 		}
 		while (i > -1)			//free stores
 		{
-			free (store);
+			free(store);
 			i--;
 		}
 	}

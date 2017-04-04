bc606beff59651ebfa3ef915d7f940ea41c152ed
warren fung <wfung@e1z2r6p1.42.us.org>
2017-04-03 22:46:31 -0700

    Mikim helped me fix my bug (REMEMBER TO PUT CORRECT DATA TYPE

--- get_next_line.c
+++ get_next_line.c
@@ -4,12 +4,12 @@
 int		get_next_line(const int fd, char **line)
 {
 	int				count;
-	char			*tmp;
+	char			tmp[BUFF_SIZE + 1];
 	static char		*buff;
 	int				read_attempt;	//for testing purposes
 
 	buff = NULL;
-	if (fd == -1 || !(tmp = ft_memalloc((size_t)BUFF_SIZE + 1)))
+	if (fd == -1 )//|| !(tmp = ft_memalloc((size_t)BUFF_SIZE + 1)))
 		return (-1);
 	read_attempt = 1;
 	printf("inside GNL; about to start\n");
@@ -17,25 +17,27 @@ int		get_next_line(const int fd, char **line)
 	{												//might be diff vs BUFF_SIZE
 		tmp[count] = '\0';
 		printf("read_attempt = [%i] count = [%i]\ntmp = %s\n", read_attempt, count, tmp);
-		buff == NULL ? buff = ft_strdup(tmp) : (buff = ft_strjoin(buff, tmp));
+		buff == NULL ? buff = ft_strdup((const char*)tmp) : (buff = ft_strjoin((const char*)buff, (const char*)tmp));
 		if (!buff)
 			return (-1);
 		printf("buff str = [%s]\n", buff);
 		if (ft_strchr(buff, 10) != NULL)
 			break;
-		ft_memalloc((size_t)tmp);		//clear tmp
+		printf("AA\n");
+		char *s = ft_memalloc((size_t)tmp);		//clear tmp		//DONT PUT DIFF TYPES AS INPUT
+		printf("BB\n");
 		read_attempt++;
 	}
 	printf("finished reading inside GNL\n");
-	free(tmp);
+/*	free(tmp);
 	ft_memccpy(&line, buff, 10, ft_strlen(buff));
 	tmp = ft_memalloc((size_t));
 	tmp = ft_strsub(buff, ft_strcmp(buff, *line), ft_strlen(buff) - ft_strcmp(buff, *line));
 	if (!tmp)
 		return (-1);
-	if (!(buff = ft_strdup(tmp)))
+*/	if (!(buff = ft_strdup(tmp)))
 		return (-1);
-	free(tmp);
+//	free(tmp);
 	printf("line str = [%s]\n", *line);
 	if (count == 0)
 		return (0);

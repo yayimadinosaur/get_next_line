760544bb5a16ccfeb3716ea4312bbd7aa91793ea
warren fung <wfung@e1z2r6p1.42.us.org>
2017-04-17 16:28:57 -0700

    added lenchk function (doesnt work anymore)

--- get_next_line_newnew.c
+++ get_next_line_newnew.c
@@ -6,16 +6,47 @@
 /*   By: wfung <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/17 15:38:21 by wfung             #+#    #+#             */
-/*   Updated: 2017/04/17 15:57:52 by wfung            ###   ########.fr       */
+/*   Updated: 2017/04/17 16:26:35 by wfung            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
 //newest
-//with multiple oes not work i ont think
-//replacing b ater every call
-//copie t_ptrlen an t_cpychr rom gnl1
+//added new function ft_lenchk (to fit norm)
+
+/*	if (ft_strlen(b) > ft_strlen(*line))
+	{
+		hold = ft_strsub(b, ft_ptrlen(b, next + 1, ft_strlen(b)), ft_strlen(b) - ft_strlen(*line));
+		free(b);
+		b = ft_strdup(hold);
+		free(hold);
+		return (1);
+	}
+*/
+int		ft_lenchk(char *s1, char *s2, char *ptr)	//b, *line, next;
+{
+	char	*hold;
+
+	hold = NULL;
+	if (ft_strlen(s1) > ft_strlen(s2))
+	{
+		hold = ft_strsub(s1, ft_ptrlen(s1, ptr + 1, ft_strlen(s1)), ft_strlen(s1) - ft_strlen(*s2));
+		free(s1);
+		s1 = NULL;
+		s1 = ft_strdup(hold);
+		free(hold);
+		hold = NULL;
+		return (1);
+	}
+//	else if (ft_strlen(s1) == ft_strlen(s2))
+//	{
+//		next = NULL;
+//		return (1);
+//	}
+	else
+		return (0);
+}
 
 int		ft_ptrlen(char *p1, char *p2, int len)
 {
@@ -57,13 +88,13 @@ char		*ft_cpychr(char *str, int c, int len)
 int				get_next_line(const int fd, char **line)
 {
 	static char		*b;
-	char			*hold;
+//	char			*hold;
 	char			*next;
 	char			tmp[BUFF_SIZE + 1];
 	int				count;
 
 	next = NULL;
-	hold = NULL;
+//	hold = NULL;
 	if (fd < 0 || BUFF_SIZE <= 0 || read(fd, tmp, 0) < 0)
 		return (-1);
 	while ((count = read(fd, tmp, BUFF_SIZE)) > 0)
@@ -73,7 +104,7 @@ int				get_next_line(const int fd, char **line)
 	}
 	(next = ft_strchr(b, 10)) != NULL ? (*line = ft_cpychr(b, 10, ft_ptrlen(b, next, ft_strlen(b))))
 		: (*line = ft_strdup(b));
-	if (ft_strlen(b) > ft_strlen(*line))
+/*	if (ft_strlen(b) > ft_strlen(*line))
 	{
 		hold = ft_strsub(b, ft_ptrlen(b, next + 1, ft_strlen(b)), ft_strlen(b) - ft_strlen(*line));
 		free(b);
@@ -81,5 +112,5 @@ int				get_next_line(const int fd, char **line)
 		free(hold);
 		return (1);
 	}
-	return (0);
+*/	return (ft_lenchk(b, *line, next));
 }
